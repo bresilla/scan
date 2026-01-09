bf6c2ed53de4a83827bcf513607acf5fc56ada60
Trim Bresilla <trim.bresilla@gmail.com>
2026-01-10 00:59:08 +0100

    feat: init

---
+++ examples/simple_input.cpp
@@ -0,0 +1,6 @@
+#include <scan/scan.hpp>
+
+int main() {
+    scan::test_function();
+    return 0;
+}
---
+++ include/scan/scan.hpp
@@ -0,0 +1,9 @@
+#pragma once
+
+#include <echo/echo.hpp>
+
+namespace scan {
+
+    void test_function() { echo::print("Hello, world!"); }
+
+} // namespace scan
